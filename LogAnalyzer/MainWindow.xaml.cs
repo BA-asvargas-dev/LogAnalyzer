c411848239f8b0a4fab7e650403bcd85f8b617e2
BA-asvargas-dev <asvargas@bancatlan.hn>
2025-02-07 11:45:32 -0600

    Problema con el uso portable de LogParser solucionado, además se agregaron cambios como un mensaje para mostrar que el proceso termino y limpiar la pantalla

--- LogAnalyzer/MainWindow.xaml.cs
+++ LogAnalyzer/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage.Pickers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -59,75 +61,91 @@ namespace LogAnalyzer
             progressBar.Value = 0;
             progressBar.Maximum = appNames.Length;
             txtLog.Text = "";
+            string? appPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (appPath == null)
+            {
+                await ShowMessage("Error", "No se pudo determinar la ruta de la aplicación.");
+                return;
+            }
+
+            string logParserPath = Path.Combine(appPath, "tools", "LogParser.exe");
+            List<string> appsWithErrors = new List<string>(); // Lista para aplicaciones con errores
 
             for (int i = 0; i < appNames.Length; i++)
             {
                 string appName = appNames[i];
                 AppendLog($"Generando informe para la aplicación: {appName}...");
 
-                // Normaliza el nombre de la aplicación eliminando caracteres no permitidos
-                // Normaliza el nombre del archivo, reemplaza espacios por guiones bajos
-                // y elimina caracteres no válidos para nombres de archivos
                 string safeAppName = new string(appName
                     .Replace(' ', '_')
                     .Normalize(NormalizationForm.FormD)
                     .Where(c => !char.IsWhiteSpace(c) && CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
                     .ToArray());
 
-                // Asegura que el nombre del archivo termina con ".csv"
                 if (!safeAppName.EndsWith(".csv"))
                 {
                     safeAppName += ".csv";
                 }
 
                 string outputFile = Path.Combine(outputFolder, $"resultados_{safeAppName}");
-                string logParserPath = Path.Combine(AppContext.BaseDirectory, "tools", "LogParser.exe");
                 string logParserCommand = $"{logParserPath} \"SELECT date AS Fecha, time AS Hora, s-sitename AS Sitio, s-ip AS IP_Servidor, " +
-                               $"cs-method AS Metodo, cs-uri-stem AS Ruta, cs-uri-query AS Consulta_Ruta, s-port AS Puerto_Servidor, " +
-                               $"cs-username AS Usuario, c-ip AS IP_Cliente, cs(User-Agent) AS UserAgent, sc-status AS Estado_Respuesta, " +
-                               $"sc-substatus AS Subestado_Respuesta, sc-win32-status AS Estado_Win32 FROM {logPath}/*.log WHERE cs-uri-stem LIKE '%{appName}%'\" " +
-                               $"-i:IISW3C -o:CSV > {outputFile}";
+                                           $"cs-method AS Metodo, cs-uri-stem AS Ruta, cs-uri-query AS Consulta_Ruta, s-port AS Puerto_Servidor, " +
+                                           $"cs-username AS Usuario, c-ip AS IP_Cliente, cs(User-Agent) AS UserAgent, " +
+                                           $"sc-status AS Estado_Respuesta, sc-substatus AS Subestado_Respuesta, " +
+                                           $"sc-win32-status AS Estado_Win32 FROM {logPath}/*.log WHERE cs-uri-stem LIKE '%{appName}%'\" " +
+                                           $"-i:IISW3C -o:CSV > {outputFile}";
 
-
-                // string logParserCommand = $"{logParserPath} \"SELECT date AS Fecha, time AS Hora, cs-uri-stem AS Ruta, sc-status AS Estado_Respuesta FROM {logPath} WHERE cs-uri-stem LIKE '%{appName}%'\" -i:IISW3C -o:CSV > {outputFile}";
-
-
+                string errorFilePath = Path.Combine(Path.GetTempPath(), "logparser_error.txt");
+                string command = $"{logParserCommand} 2> {errorFilePath}";
 
                 try
                 {
                     var processStartInfo = new ProcessStartInfo
                     {
                         FileName = "cmd.exe",
-                        Arguments = $"/c {logParserCommand}",
+                        Arguments = $"/C {logParserCommand}",
                         CreateNoWindow = true,
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
                         RedirectStandardError = true
                     };
 
-                    var process = Process.Start(processStartInfo);
-                    string output = await process.StandardOutput.ReadToEndAsync();
-                    string errors = await process.StandardError.ReadToEndAsync();
-                    await process.WaitForExitAsync();
+                    using (var process = Process.Start(processStartInfo))
+                    {
+                        if (process == null)
+                        {
+                            AppendLog($"Error al iniciar LogParser para {appName}");
+                            appsWithErrors.Add(appName);
+                            continue;
+                        }
 
-                    AppendLog(output);
-                    if (!string.IsNullOrWhiteSpace(errors))
-                    {
-                        AppendLog($"Errores: {errors}");
-                    }
+                        process.WaitForExit(); // Asegura que LogParser termine antes de continuar
+                        string output = await process.StandardOutput.ReadToEndAsync();
+                        string errors = await process.StandardError.ReadToEndAsync();
+                        await process.WaitForExitAsync();
 
-                    if (File.Exists(outputFile))
-                    {
-                        AppendLog($"Informe generado exitosamente para {appName}. Guardado en: {outputFile}");
-                    }
-                    else
-                    {
-                        AppendLog($"No se generó el informe para {appName}.");
+                        AppendLog(output);
+                        if (!string.IsNullOrWhiteSpace(errors))
+                        {
+                            AppendLog($"Errores: {errors}");
+                            appsWithErrors.Add(appName);
+                        }
+
+                        if (File.Exists(outputFile))
+                        {
+                            AppendLog($"Informe generado exitosamente para {appName}. Guardado en: {outputFile}");
+                        }
+                        else
+                        {
+                            AppendLog($"No se generó el informe para {appName}.");
+                            appsWithErrors.Add(appName);
+                        }
                     }
                 }
                 catch (Exception ex)
                 {
                     AppendLog($"Error al ejecutar LogParser para {appName}: {ex.Message}");
+                    appsWithErrors.Add(appName);
                 }
 
                 progressBar.Value = i + 1;
@@ -136,13 +154,25 @@ namespace LogAnalyzer
 
             AppendLog("Todos los reportes han sido generados.");
             progressBar.Visibility = Visibility.Collapsed;
+
+            // Verificar si hubo errores y mostrar el mensaje correspondiente
+            string message = appsWithErrors.Any()
+                ? $"Hubo errores al generar los informes para las siguientes aplicaciones: {string.Join(", ", appsWithErrors)}."
+                : "Todos los informes se generaron exitosamente.";
+
+            await ShowMessage("Proceso Completo", message);
+
+            // Limpiar la pantalla
+            ClearScreen();
         }
 
-        private void AppendLog(string message)
+        private void ClearScreen()
         {
-            txtLog.Text += message + Environment.NewLine;
-            txtLog.Select(txtLog.Text.Length, 0);
-            txtLog.Focus(FocusState.Programmatic);
+            txtLog.Text = "";
+            txtAppNames.Text = "";
+            txtLogPath.Text = "";
+            txtOutputFolder.Text = "";
+            progressBar.Value = 0;
         }
 
         private async Task ShowMessage(string title, string content)
@@ -156,6 +186,13 @@ namespace LogAnalyzer
             };
 
             await dialog.ShowAsync();
+        }
+
+        private void AppendLog(string message)
+        {
+            txtLog.Text += message + Environment.NewLine;
+            txtLog.Select(txtLog.Text.Length, 0);
+            txtLog.Focus(FocusState.Programmatic);
         }
 
     }
